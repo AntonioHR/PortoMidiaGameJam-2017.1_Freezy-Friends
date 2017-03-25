f59a60db30c6da45a253e39d74803b4713d4801f
vinicius emanuel <vinicius.emanuel@sodet.biz>
2017-03-25 14:22:49 -0300

    prototype scene

---
+++ bola_de_neve/Assets/Scripts/FolowObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowObj : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate(){
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, 1f);
+	}
+}
